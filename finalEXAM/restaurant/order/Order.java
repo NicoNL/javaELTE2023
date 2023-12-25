83d2a69bf0826f42ba95b6c42ca69aba5c7a4634
NicoNL <a0t4zr@inf.elte.hu>
2023-12-25 23:25:08 +0100

    Order class part1

--- finalEXAM/restaurant/order/Order.java
+++ finalEXAM/restaurant/order/Order.java
@@ -4,7 +4,52 @@ public class Order
 {
     public OrderType type;
     public String item;
-    public boolean isInMenu;
-    
+    public final boolean isInMenu;
+    private final int price = 2490;
+    private final int orderNumber = 1;
+    static private int nextOrderNumber = 1;
+    private boolean isDone = false;
+
+    public Order(OrderType type, String item, boolean isInMenu)
+    {
+        this.type = type;
+        this.item = item;
+        this.isInMenu = isInMenu;
+        nextOrderNumber++;
+        int finalPrice = getPrice();
+    }
+
+    public void make()
+    {
+        isDone = true;
+    }
+
+    public boolean isDone()
+    {
+        if(isDone == true)
+        {
+            throw new IllegalStateException("Hamburger is Done");
+        }
+        else
+        {
+            return isDone;
+        }
+    }
+
+    // GETTERS AND SETTER
+
+    public int getPrice()
+    {
+        int finalPrice = this.price;
+        if(isInMenu == true)
+        {
+            finalPrice += 590;
+        }
+        if(type == OrderType.DELIVERY)
+        {
+            finalPrice += 990;
+        }
+        return finalPrice;
+    }
     
 }
